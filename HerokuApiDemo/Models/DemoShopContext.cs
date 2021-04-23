8ea1df9b8e2a520e58ec5890521adaf32ba9f621
Cristopher Coronado Moreira <cristofima@hotmail.com>
2021-04-22 20:53:12 -0500

    Init project

---
+++ HerokuApiDemo/Models/DemoShopContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace HerokuApiDemo.Models
+{
+    public partial class DemoShopContext : DbContext
+    {
+        public DemoShopContext()
+        {
+        }
+
+        public DemoShopContext(DbContextOptions<DemoShopContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=DemoShop;Username=postgres;Password=12345678");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Spanish_Ecuador.1252");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Enabled)
+                    .IsRequired()
+                    .HasDefaultValueSql("true");
+
+                entity.Property(e => e.ExpirationDate).HasColumnType("date");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Price).HasPrecision(4, 2);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ HerokuApiDemo/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HerokuApiDemo.Models
+{
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public bool Enabled { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public string Description { get; set; }
+    }
+}
