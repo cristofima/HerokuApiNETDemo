47d08a079eb6aa9e4870e1e36a4bb5adabc084fa
Cristopher Coronado Moreira <cristofima@hotmail.com>
2021-04-24 17:58:19 -0500

    Adding DataAnnotations to product fields

--- HerokuApiDemo/Models/Product.cs
+++ HerokuApiDemo/Models/Product.cs
@@ -1,15 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HerokuApiDemo.Models
 {
     public partial class Product
     {
         public int ProductId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
         public string Name { get; set; }
+
+        [Required]
         public decimal Price { get; set; }
+
+        [Required]
         public int Stock { get; set; }
+
         public bool Enabled { get; set; }
-        public DateTime? ExpirationDate { get; set; }
+
+        [Required]
+        public DateTime ExpirationDate { get; set; }
         public string Description { get; set; }
     }
 }
